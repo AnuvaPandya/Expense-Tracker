ad8fdcc71e9ccada82f8107fff57445aae933a81
AnuvaPandya <pandyanuva96@gmail.com>
2021-12-04 00:55:41 +0530

    filter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,36 +1,43 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <div>
-      <ExpenseFilter />
-    
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amt={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amt={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amt={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amt={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.items[0].title}
+          amt={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amt={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amt={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amt={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </Card>
     </div>
   );
-}
+};
 export default Expenses;
